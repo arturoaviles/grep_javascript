7d81c0664bf77fcc894d6272f7a1a449b49dd00c
Arturo Avilés <artcastell@gmail.com>
2015-10-03 02:12:10 -0500

    Grep.js can now prompt the user for text and a word and print each line in which the word appears in text

--- grep.js
+++ grep.js
@@ -1,9 +1,12 @@
+// Start of the Program
+console.log('The program started');
+
 // Initial variables
-var textArea = get();
-var searchWord = get();
+var textArea = prompt('Paste the text in which you want to search for a word: ');
+var searchWord = prompt('Write the word you want to search: ');
 
 // Divide the textarea by lines
-var lines = $(textArea).val().split('\n');
+var lines = textArea.split('\n');
 
 // To LowerCase
 
@@ -17,11 +20,16 @@ Iterates line by line
 	Validates if searchWord is in those words 
 	Prints an html <br> tag for each line that it finds the word
 */ 
-for(var i = 0; i == lines.length; i++) {
-	var words = $(lines).val().split(' ');
-	for (var j = 0; j >= words.length; j++) {
+for(var i = 0; i < lines.length; i++){
+	var words = lines[i].split(' ');
+	for(var j = 0; j <= words.length; j++) {
 		if(searchWord==words[j]){
-			print("<br>"+lines[i]);
-		};
-	};
-};
+			//If word was found print: index +1 (Remember array starts on 0) and  print line.
+			var index = lines.indexOf(lines[i]) + 1;
+			document.write("<h2>Line "+index+": "+lines[i]);
+		}
+	}
+}
+
+// End of program
+console.log('The program ended correctly');
